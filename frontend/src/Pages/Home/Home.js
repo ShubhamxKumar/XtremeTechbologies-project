252a5441398ac2b6ef6c897fd49ae879ad62d77a
zhcet19 <faizamu19@gmail.com>
2021-12-09 23:48:57 +0530

    points plotting logic added

--- frontend/src/Pages/Home/Home.js
+++ frontend/src/Pages/Home/Home.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect ,useMemo} from 'react';
+import Globe from "react-globe.gl";
 
 function Home() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const [data, setdata] = useState([])
+  const [arcdata,setarcdata]=useState([])
+
+  const onGlobeClick = ({ lat, lng }, event) => {
+
+    setdata((prev) => [
+      ...prev,
+      { lat: lat,
+        lng: lng,  
+        size: 0,
+        color: "red",
+        radius: 1.0,
+        name:data.length+''
+        },
+    ]);
+    if(data.length>=1)
+    {
+    
+      setarcdata((prev)=>[
+        ...prev,
+        
+          {
+            startLat: data[data.length-1].lat,
+            startLng: data[data.length-1].lng,
+            endLat: lat,
+            endLng: lng,
+            color: " white"
+          }
+        
+      ]);
+
+    }
+    //setdata(temp);
+    console.log(data);
+  };
+ const onPointClick=((point,event)=>{
+   console.log(point);
+ })
+  return <Globe
+    globeImageUrl="//unpkg.com/three-globe/example/img/earth-day.jpg"
+    bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+    pointsData={data}
+    onGlobeClick={onGlobeClick}
+    pointAltitude={0}
+    arcAltitude={0}
+    arcsData={arcdata}
+    onPointClick={onPointClick}
+    pointColor={() => 'orange'}
+
+  />
+  
 }
 
 export default Home
